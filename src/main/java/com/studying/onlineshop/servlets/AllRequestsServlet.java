dba2890521eed035f9aa5cb4db140fce23ebcb1f
Nataliia <ruba.fish1500@gmail.com>
2023-06-19 16:57:22 +0300

    some templates added

---
+++ src/main/java/com/studying/onlineshop/servlets/AllRequestsServlet.java
@@ -0,0 +1,19 @@
+package com.studying.onlineshop.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AllRequestsServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
